f89178aeb2743e2755246381c5954fc08553f415
Steinhof <steinhof808@gmail.com>
2019-11-04 13:32:24 +0300

    loading state fix

--- src/client/modules/containers/ProductsList/ProductsList.tsx
+++ src/client/modules/containers/ProductsList/ProductsList.tsx
@@ -2,10 +2,11 @@ import React, { ReactElement } from 'react';
 import usePromise from 'react-promise';
 import { connect } from 'react-redux';
 import Product from '../../components/Product/Product';
+import { PostObject } from '../../../../types/database';
 
 // import { StateToProps } from '../../../../types/store/store';
 
-function ProductsList({ products }): ReactElement {
+function ProductsList({ products }: PostObject): ReactElement | null {
     const { value, loading } = usePromise<string>(products);
 
     // Removes loading spinner when fetch finished
@@ -16,20 +17,20 @@ function ProductsList({ products }): ReactElement {
         }
     };
 
+    if (loading) return null;
+
     return (
         <div className="product__container">
-            {!loading
-                ? value.map(item => (
-                      <Product
-                          key={item._id}
-                          id={item._id}
-                          name={item.name}
-                          description={item.description}
-                          price={item.price}
-                          date={item.date}
-                      />
-                  ))
-                : null}
+            {value.map(item => (
+                <Product
+                    key={item._id}
+                    id={item._id}
+                    name={item.name}
+                    description={item.description}
+                    price={item.price}
+                    date={item.date}
+                />
+            ))}
             {removeSpinnerHandler()}
         </div>
     );
